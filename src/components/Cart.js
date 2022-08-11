8074bab56391f460751a8a34a7c7e58b04afcd1e
Haim Yeshurun <haimyess@gmail.com>
2022-08-11 10:51:54 +0300

    added components

---
+++ src/components/Cart.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react";
+
+function Cart() {
+  return <h1>Cart Page</h1>;
+}
+
+export default Cart;
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+/** @format */
+
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/shop'>Shop</Link>
+        <Link to='/cart'>Cart</Link>
+      </nav>
+    </>
+  );
+}
+
+export default Nav;
